1aab6019b6fda3e47cea25bcf2b387ce2dc8635e
Javier Steinaker <jsteinaker@gmail.com>
2017-06-29 16:10:20 +0200

    Typo

--- contrib/appveyor-after-build.bat
+++ contrib/appveyor-after-build.bat
@@ -1,6 +1,6 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
 	echo "Packaging Release zip"
-	if "%PLATFORM%" == "*x86_64" (
+	if "%PLATFORM%" == "x86_64" (
         7z a win32yank-x64.zip LICENSE README.md %APPVEYOR_BUILD_FOLDER%\target\release\win32yank.exe
 	)
 	if "%PLATFORM%" == "i686" (
