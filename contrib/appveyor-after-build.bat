8cceb3daf5e016faeaebf4ebaf88692781c685d1
Javier Steinaker <jsteinaker@gmail.com>
2017-06-29 15:59:00 +0200

    Appveyor fixes #4

--- contrib/appveyor-after-build.bat
+++ contrib/appveyor-after-build.bat
@@ -1,9 +1,9 @@
 if "%APPVEYOR_REPO_TAG%" == "true" (
 	echo "Packaging Release zip"
-	if "%PLATFORM%" == "*x86_64-pc-windows-msvc.exe" (
+	if "%PLATFORM%" == "*x86_64" (
         7z a win32yank-x64.zip LICENSE README.md %APPVEYOR_BUILD_FOLDER%\target\release\win32yank.exe
 	)
-	if "%PLATFORM%" == "*i686-pc-windows-msvc.exe" (
+	if "%PLATFORM%" == "i686" (
         7z a win32yank-x86.zip LICENSE README.md %APPVEYOR_BUILD_FOLDER%\target\release\win32yank.exe
 	)
 )
